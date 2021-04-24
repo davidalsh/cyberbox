398da4dbf90df8b06a5401aa17c786a3ea93b245
David <softw.dev.dave@gmail.com>
2021-04-25 00:31:21 +0200

    Sign In/Up templates added, working with views for auth forms

--- cyberbox/main/urls.py
+++ cyberbox/main/urls.py
@@ -3,4 +3,6 @@ from main.views import *
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='home'),
+    path('sign-in/', SignInView.as_view(), name='sign-in'),
+    path('sign-up/', SignUpView.as_view(), name='sign-up'),
 ]
--- cyberbox/main/views.py
+++ cyberbox/main/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
+from main.forms import LoginForm
+
+from django.contrib.auth import authenticate, login
 
 
 class MainPageView(View):
     def get(self, request):
-        return render(request, 'main/login.html')
+        return render(request, 'main/index.html')
+
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        context = {
+            'form': form,
+        }
+        return render(request, 'main/sign_in.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        print(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+        return render(request, 'main/sign-in.html', {'form': form})
+
+
+class SignUpView(View):
+    pass
+    # def get(self, request):
+    #     return render(request, 'main/sign_up.html')

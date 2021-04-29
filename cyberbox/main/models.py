a2ef292a659a237dd6afcf0bff454e69930a3be0
David <softw.dev.dave@gmail.com>
2021-04-29 12:42:18 +0200

    Added ProgLang model; first branch coomit

--- cyberbox/main/models.py
+++ cyberbox/main/models.py
@@ -2,11 +2,64 @@ from django.db import models
 
 
 '''
-
 Models:
 
 
+User
+-------------------
+username
+email
+password
+avatar
+about
+github
+linkedin
+
+programming_languages = ManyToMany(ProgrammingLanguages)
+
+projects = ManyToMany(Project)
+
+working_place = ForeignKey()
+===================
 
 
+WorkingPlace
+-------------------
 
+
+===================
+
+#projects app
+Project
+-------------------
+title
+description
+languages
+image
+date
+
+full = Booleanfield(default=True)
+
+
+working_place in projects = ManyToMany(User)
+users = ManyToMany(User)
+
+===================
 '''
+
+
+class ProgrammingLanguages(models.Model):
+    title = models.CharField(max_length=80)
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "ProgrammingLanguage"
+        verbose_name_plural = "ProgrammingLanguages"
+        ordering = ['-title']
+
+
+
+
